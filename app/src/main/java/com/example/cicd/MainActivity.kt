3559e7e7d19ce401e1c1fdaf2ba60ea2957ce8f4
umedjj <hasanzodaumed60@gmail.com>
2025-06-14 02:24:30 +0500

    test

--- app/src/main/java/com/example/cicd/MainActivity.kt
+++ app/src/main/java/com/example/cicd/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello GGG $name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
